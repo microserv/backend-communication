cc479072668c55c28c93cc25e5fbb5e0b97fe0fa
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-02-24 16:00:16 +0100

    Make the install script install opendht related libraries, since we will try to do this instead of using ZMQ. The reason for doing this, is that it is more robust, and mostly simpler.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
+echo "Installing dependencies..."
+sudo apt-get install libncurses5-dev libreadline-dev nettle-dev libgnutls28-dev build-essential cmake
+sudo apt-get install cython3 python3-dev python3-setuptools
 
-set -e
-echo "This script assumes that you are using debian or ubuntu."
-echo -e "The following packages will be installed: libzmq3\n"
+### Install msgpack-c 1.3 ###
+echo "Installing msgpack..."
+wget https://github.com/msgpack/msgpack-c/releases/download/cpp-1.3.0/msgpack-1.3.0.tar.gz
+tar -xzf msgpack-1.3.0.tar.gz
+cd msgpack-1.3.0 && mkdir build && cd build
+cmake -DMSGPACK_CXX11=ON -DMSGPACK_BUILD_EXAMPLES=OFF -DCMAKE_INSTALL_PREFIX=/usr ..
+make -j
+sudo make install
 
-sudo apt-get install libzmq3 libzmq3-dev
 
-echo "All done!"
+### Install openhdt ###
+echo "Installing opendht"
+git clone https://github.com/savoirfairelinux/opendht.git
+
+# build and install
+cd opendht
+mkdir build && cd build
+cmake -DOPENDHT_PYTHON=ON -DCMAKE_INSTALL_PREFIX=/usr ..
+make -j
+sudo make install

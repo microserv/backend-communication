0d36b2ff7ad8561e9621bd190ac8004b6aaac16e
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-02-25 14:38:53 +0100

    Ensure that the install script cleans up after itself.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@ echo "Installing dependencies..."
 sudo apt-get install libncurses5-dev libreadline-dev nettle-dev libgnutls28-dev build-essential cmake
 sudo apt-get install cython3 python3-dev python3-setuptools
 
+mkdir tmp_build
+cd tmp_build
+
+cwd=$(pwd)
+
 ### Install msgpack-c 1.3 ###
 echo "Installing msgpack..."
 wget https://github.com/msgpack/msgpack-c/releases/download/cpp-1.3.0/msgpack-1.3.0.tar.gz
@@ -11,14 +16,19 @@ cmake -DMSGPACK_CXX11=ON -DMSGPACK_BUILD_EXAMPLES=OFF -DCMAKE_INSTALL_PREFIX=/us
 make -j
 sudo make install
 
-
 ### Install openhdt ###
+cd $cwd
 echo "Installing opendht"
 git clone https://github.com/savoirfairelinux/opendht.git
 
-# build and install
 cd opendht
 mkdir build && cd build
 cmake -DOPENDHT_PYTHON=ON -DCMAKE_INSTALL_PREFIX=/usr ..
 make -j
 sudo make install
+
+cd $cwd
+cd ..
+
+echo "Deleting build directory..."
+rm -rf tmp_build

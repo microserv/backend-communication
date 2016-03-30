cb6c33b9a892b64ab945e6ed321bab61c5240bd7
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-03-30 15:49:23 +0200

    Ensure that a node can be created without having a logger.

--- src/node.py
+++ src/node.py
@@ -17,36 +17,36 @@ class Node(EntangledNode):
         super(self.__class__, self).__init__(udpPort=port)
 
         self.logger = logger
-        self.logger.info('Creating Entangled Node on %s:%d...' % (self.get_ip("wlan0"), port))
+
+        if self.logger:
+            self.logger.info('Creating Entangled Node on %s:%d...' % (get_ip("wlan0"), port))
 
         # A bootstrap node consists of an IP and port to a known node in the
         # network we want to join.
         if bootstrap_node:
-            self.logger.info("Connecting to network through: %s:%s" % (bootstrap_node[0], bootstrap_node[1]))
+            if self.logger:
+                self.logger.info("Connecting to network through: %s:%s" % (bootstrap_node[0], bootstrap_node[1]))
 
-            formatted_bootstrap_node = self._format_bootstrap_node_info(bootstrap_node)
+            formatted_bootstrap_node = format_bootstrap_node_info(bootstrap_node)
             self.joinNetwork([formatted_bootstrap_node])
         else:
             self.joinNetwork()
 
-        logger.info('Node created!')
+        if self.logger:
+            logger.info('Node created!')
 
-    def _format_bootstrap_node_info(self, node_info):
-        """
-        Format lists of the format [IP, Port] into tuples readable by an
-        Entangled node.
-        """
-        return (node_info[0], int(node_info[1]))
 
     def handle_error(self, failure):
-        self.logger.error('An error has occurred:', failure.getErrorMessage())
+        if self.logger:
+            self.logger.error('An error has occurred:', failure.getErrorMessage())
 
     def get_value(self, key):
         """
         Attempt to obtain a value from the DHT.
         The key 'key' is a string of arbitrary length.
         """
-        self.logger.info('Retrieving "%s"...' % key)
+        if self.logger:
+            self.logger.info('Retrieving "%s"...' % key)
         # Query the other nodes in the network to determine whether they have
         # this key.
         deferred_result = self.iterativeFindValue(key)
@@ -58,7 +58,8 @@ class Node(EntangledNode):
         """
         Attempt to delete a value from the DHT.
         """
-        self.logger.info('\nDeleting: "%s"' % key)
+        if self.logger:
+            self.logger.info('\nDeleting: "%s"' % key)
         # Query the other nodes in the network to find and delete the key/value
         # pair.
         deferred_result = self.iterativeDelete(key)
@@ -71,7 +72,8 @@ class Node(EntangledNode):
         Stores the specified value in the DHT using the specified key.
         The key 'key' and value 'value' must both be strings.
         """
-        self.logger.info('Soring "%s" -> "%s"' % (key, value))
+        if self.logger:
+            self.logger.info('Soring "%s" -> "%s"' % (key, value))
         # Attempt to find the closest neighbour node and store the value there.
         # If no nodes are close enough, store it in this node.
         deferred_result = self.iterativeStore(key, value)

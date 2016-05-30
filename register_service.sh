b40f903c9f71d69de821f7a4026224a69f3b7c7a
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-05-30 06:26:43 -0400

    Ensure that the service registration script retries if it fails.

--- register_service.sh
+++ register_service.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sleep 2
-curl -i -H "Content-Type: application/json" -X POST -d '{"service_name":"'"$SERVICE_NAME"'"}' http://localhost:9001/register
+curl -S --retry-max-time 10 --retry 5 --retry-delay 5 -i -H "Content-Type: application/json" -X POST -d '{"service_name":"'"$SERVICE_NAME"'"}' http://localhost:9001/register
